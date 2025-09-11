9c50c29e8b849d4ce570508d155bdcb3ab2a7937
alfahrelrifananda <pahrel1234@gmail.com>
2025-09-11 19:41:27 +0700

    change

--- Program.cs
+++ Program.cs
@@ -1,12 +1,24 @@
-string nama = "Fahrel";
-int umur = 20;
-bool mhs = true;
-double money = 1000.50;
-char initial = 'F';
+using System;
 
-System.Console.WriteLine("Hallo, Welt!");
-System.Console.WriteLine("Ich heiße " + nama);
-System.Console.WriteLine("Mein Anfangsbuchstabe ist " + initial);
-System.Console.WriteLine("Ich bin " + umur + " Jahre alt");
-System.Console.WriteLine("Ich bin Student: " + mhs);
-System.Console.WriteLine("Ich habe " + money + " Rupiah");
+public class Program
+{
+    public static void Main(string[] args)
+    {
+        string nama = "Fahrel";
+        int umur = 20;
+        bool mhs = true;
+        double money = 1000.50;
+        char initial = 'F';
+
+        System.Console.WriteLine("Hallo, Welt!");
+        System.Console.WriteLine("Ich heiße " + nama);
+        System.Console.WriteLine("Mein Anfangsbuchstabe ist " + initial);
+        System.Console.WriteLine("Ich bin " + umur + " Jahre alt");
+        System.Console.WriteLine("Ich bin Student: " + mhs);
+        System.Console.WriteLine("Ich habe " + money + " Rupiah");
+        System.Console.WriteLine();
+        System.Console.Write("Say your name : ");
+        string input = Console.ReadLine();
+        System.Console.WriteLine("Hallo " + input);
+    }
+}
