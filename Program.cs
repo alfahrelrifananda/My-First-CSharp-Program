3d00258d62e514ab8eb7e076c79b32bbbea53e72
alfahrelrifananda <pahrel1234@gmail.com>
2025-09-11 18:27:17 +0700

    First Commit

---
+++ Program.cs
@@ -0,0 +1,11 @@
+string nama = "Fahrel";
+int umur = 20;
+bool mhs = true;
+double money = 1000.50;
+char initial = 'F';
+
+System.Console.WriteLine("Ich heiße " + nama);
+System.Console.WriteLine("Mein Anfangsbuchstabe ist " + initial);
+System.Console.WriteLine("Ich bin " + umur + " Jahre alt");
+System.Console.WriteLine("Ich bin Student: " + mhs);
+System.Console.WriteLine("Ich habe " + money + " Rupiah");
