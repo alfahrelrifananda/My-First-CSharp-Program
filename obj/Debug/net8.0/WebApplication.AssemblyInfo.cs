f71ddc48c07479d9b881cb0fee6fd1a5e282fa79
alfahrelrifananda <pahrel1234@gmail.com>
2025-09-12 19:57:54 +0700

    Inggris kita linggis

--- obj/Debug/net8.0/WebApplication.AssemblyInfo.cs
+++ obj/Debug/net8.0/WebApplication.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("WebApplication")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+f9544df2d148cf26230983afdd2c966e80fcba47")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+a5f4d95f93ef85cbe61908490aea830e1502f48f")]
 [assembly: System.Reflection.AssemblyProductAttribute("WebApplication")]
 [assembly: System.Reflection.AssemblyTitleAttribute("WebApplication")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
